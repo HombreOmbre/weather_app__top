dbae627d17abfb5ba30987ad0503520171fd79d0
HombreOmbre <marcin.obrebowski@outlook.com>
2023-09-13 20:45:22 +0200

    Add new file for initialization swiper.js for daily and hourly forecasts

--- src/js/swiperController.js
+++ src/js/swiperController.js
@@ -3,18 +3,14 @@ import Swiper from 'swiper';
 const swiperController = () => {
     const addDailyForecastSwiper = () => {
         return new Swiper('.swiper-daily', {
-            slidesPerView: 1,
+            slidesPerView: 1.5,
             spaceBetween: 10,
             breakpoints: {
-                '@0.00': {
-                    slidesPerView: 1,
-                    spaceBetween: 10,
-                },
-                '@0.35': {
+                450: {
                     slidesPerView: 2,
                     spaceBetween: 10,
                 },
-                '@0.50': {
+                650: {
                     slidesPerView: 3,
                     spaceBetween: 10,
                 },
@@ -22,8 +18,34 @@ const swiperController = () => {
         });
     };
 
+    const addHourlyForecastSwiper = () => {
+        return new Swiper('.swiper-hourly', {
+            slidesPerView: 2.5,
+            spaceBetween: 10,
+            breakpoints: {
+                350: {
+                    slidesPerView: 3.5,
+                    spaceBetween: 10,
+                },
+                450: {
+                    slidesPerView: 4.5,
+                    spaceBetween: 10,
+                },
+                550: {
+                    slidesPerView: 5.5,
+                    spaceBetween: 10,
+                },
+                650: {
+                    slidesPerView: 6.5,
+                    spaceBetween: 20,
+                },
+            },
+        });
+    };
+
     return {
         addDailyForecastSwiper,
+        addHourlyForecastSwiper,
     };
 };
 
